6e8ea0426b0683020cdb437a0b8b72e1233041c5
sofiesana <s.sananikone@student.rug.nl>
2024-05-16 21:37:56 +0200

    undoing all the cpu stuff smh

--- quantus/metrics/faithfulness/irof.py
+++ quantus/metrics/faithfulness/irof.py
@@ -291,10 +291,11 @@ class IROF(Metric[List[float]]):
 
         # Move x to CPU and convert to NumPy array for segmentation
         cpu_numpy_x = x.cpu().numpy()
+        gpu_numpy_x = x
 
         # Segment image.
         segments = utils.get_superpixel_segments(
-            img=np.moveaxis(cpu_numpy_x, 0, -1).astype("double"),
+            img=np.moveaxis(gpu_numpy_x, 0, -1).astype("double"),
             segmentation_method=self.segmentation_method,
         )
         nr_segments = len(np.unique(segments))
@@ -314,12 +315,13 @@ class IROF(Metric[List[float]]):
         for i_ix, s_ix in enumerate(s_indices):
             # Move x_prev_perturbed to CPU and convert to NumPy array for perturbation
             x_prev_perturbed_cpu = x_prev_perturbed.cpu().numpy()
+            x_prev_perturbed_gpu = x_prev_perturbed.gpu().numpy()
 
             # Perturb input by indices of attributions.
             a_ix = np.nonzero((segments == s_ix).flatten())[0]
 
             x_perturbed = self.perturb_func(
-                arr=x_prev_perturbed_cpu,
+                arr=x_prev_perturbed_gpu,
                 indices=a_ix,
                 indexed_axes=self.a_axes,
             )
@@ -328,15 +330,15 @@ class IROF(Metric[List[float]]):
             )
 
             # Convert x_perturbed back to a PyTorch tensor and move to GPU
-            x_perturbed_tensor = torch.from_numpy(x_perturbed).to(x.device)
+            # x_perturbed_tensor = torch.from_numpy(x_perturbed).to(x.device)
 
             # Predict on perturbed input x.
-            x_input = model.shape_input(x_perturbed_tensor, x_perturbed_tensor.shape, channel_first=True)
+            x_input = model.shape_input(x_perturbed, x_perturbed.shape, channel_first=True)
             y_pred_perturb = model.predict(x_input)[:, y].astype(float).sum()
 
             # Normalize the scores to be within range [0, 1].
             preds.append(float(y_pred_perturb / y_pred))
-            x_prev_perturbed = x_perturbed_tensor
+            x_prev_perturbed = x_perturbed
 
         # Calculate the area over the curve (AOC) score.
         aoc = len(preds) - utils.calculate_auc(np.array(preds))
