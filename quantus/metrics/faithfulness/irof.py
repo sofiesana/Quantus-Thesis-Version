1e2680f63b044cdf63bfbe30d87b59963f2813a9
sofiesana <s.sananikone@student.rug.nl>
2024-05-20 21:28:00 +0200

    expluding plot for testing

--- quantus/metrics/faithfulness/irof.py
+++ quantus/metrics/faithfulness/irof.py
@@ -381,13 +381,12 @@ class IROF(Metric[List[float]]):
         tmpdir = os.environ.get('TMPDIR')
         results_dir = os.path.join(tmpdir, 'results')
 
-        plt.figure()
-        plt.plot(range(len(preds)), preds, marker='o')
-        plt.title('AOC Curve')
-        plt.xlabel('Number of Segments Removed')
-        plt.ylabel('Class' + self.class_name + ' Score')
-        plt.grid(True)
-        plt.savefig(results_dir + '/' + self.class_name + '_irof.png')
+        # plt.plot(range(len(preds)), preds, marker='o')
+        # plt.title('AOC Curve')
+        # plt.xlabel('Number of Segments Removed')
+        # plt.ylabel('Class' + self.class_name + ' Score')
+        # plt.grid(True)
+        # plt.savefig(results_dir + '/' + self.class_name + '_irof.png')
 
         return aoc
 
