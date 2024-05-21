8686f23f78dcd0a7deb30c1e0644ad99302fd146
sofiesana <s.sananikone@student.rug.nl>
2024-05-21 16:02:20 +0200

    fixed numpy

--- quantus/metrics/faithfulness/irof.py
+++ quantus/metrics/faithfulness/irof.py
@@ -289,7 +289,7 @@ class IROF(Metric[List[float]]):
         y[y == 255] = 0
         filtered_pred = y_pred_reshaped[torch.arange(y.shape[0]), y]
         y_pred = filtered_pred[class_category_mask]
-        y_pred = y_pred.cpu().numpy()
+        y_pred = y_pred.cpu
         
         # get average score
         y_pred = np.mean(y_pred)
